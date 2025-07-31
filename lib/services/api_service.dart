e93b09e3c80dd82c8d37684ed15fa0f982286b45
Aissam Ahmed <aissam97aitahmed@gmail.com>
2025-07-31 18:05:12 +0100

    ad

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import '../models/movie.dart';
 
 class ApiService {
   static const String _baseUrl = 'https://api.themoviedb.org/3/';
-  static const String _token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NDJiOWM0OTViYTgxMzg0NDMzZTdlMDE4NTk0ZDAxYyIsIm5iZiI6MTc1MjI0MTk5MS43NjEsInN1YiI6IjY4NzExNzQ3NTUwNGY2Y2Q5MGVkNzVkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7olUkxaBl_RuB_Z26iBIxuvlpLj3a-mZa47cAxuObfc'; // استبدله
+  static const String _token = 'YOUR_TOKEN_HERE'; 
 
   static Future<List<Movie>> fetchPopularMovies() async {
     final response = await http.get(
